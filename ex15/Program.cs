ce764ad7d11621af1b984896e137af77a413d8d9
Полина Галкина <polygal@inbox.ru>
2023-03-01 23:48:54 +0300

    added ex15

---
+++ ex15/Program.cs
@@ -0,0 +1,34 @@
+﻿// программа, которая принимает цыфру, обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Clear(); 
+int n; 
+        while(true) 
+        { 
+            Console.WriteLine("Введите чиcло от 1 до 7");   
+            if (int.TryParse(Console.ReadLine(), out n)) 
+            { 
+             
+                 
+                if (n > 0 && n < 8)
+                {  
+                   if (n > 5) 
+                   { 
+                    Console.WriteLine(n + "->" + " Да"); 
+break;
+                   } 
+                   else 
+                   { 
+                    Console.WriteLine(n + "->" + " Нет"); 
+break;
+                   } 
+                     
+                }  
+                else  
+                {  
+                    Console.WriteLine("Ошибка ввода: Введите число от 1 до 7.");     
+                } 
+            } 
+            else 
+            { 
+            Console.WriteLine("Ошибка ввода!: Введите число типа int");  
+            } 
+        }
