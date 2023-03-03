a8e7be70a175db60e9fd787f50e49bed1d2062d1
Полина Галкина <polygal@inbox.ru>
2023-03-03 19:37:40 +0300

    added ex13

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,1 +1,41 @@
 ﻿// программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int Promt(string message)
+{
+
+Console.Write(message);
+string value = Console.ReadLine();
+int result = Convert.ToInt32(value);
+return result;
+
+}
+
+int GetThirdRank(int number)
+{ 
+    while (number > 999);
+    {
+        number = number % 10; 
+    }
+return number  % 10;
+
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+       Console.WriteLine("Третьей цифры нет"); 
+       return false;
+    }
+    return true;
+}
+
+int number = Promt("Введите число > ");
+if (ValidateNumber(number))
+{
+     Console.WriteLine(number + "->" + GetThirdRank(number)); 
+}
+
+       
